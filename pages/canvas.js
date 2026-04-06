cb98bcd395629c4f8a636e16a221f818d31eb65a
sodanhama <brainosoham@gmail.com>
2026-04-06 11:54:07 +0530

    added canvas

---
+++ pages/canvas.js
@@ -0,0 +1,34 @@
+window.addEventListener('load', () => {
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d');
+
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    let drawing = false;
+
+    function startPosition(e) {
+        drawing = true;
+        draw(e)
+    }
+    function finishedPosition() {
+        drawing=false;
+        ctx.beginPath();
+    }
+
+    function draw(e) {
+        if (!drawing) return;
+        ctx.lineWidth = 10;
+        ctx.lineCap = 'round';
+        ctx.strokeStyle = 'silver';
+        ctx.lineTo(e.clientX, e.clientY);
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.moveTo(e.clientX, e.clientY);
+
+    }
+
+    canvas.addEventListener('mousedown', startPosition);
+    canvas.addEventListener('mouseup', finishedPosition);
+    canvas.addEventListener('mousemove', draw);
+});
